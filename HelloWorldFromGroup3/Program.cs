cf9c1cde1158a3271ab6b5c2cbdb35269c3bfc98
sattiganal <shyam.attiganal@gmail.com>
2020-06-20 11:45:48 -0400

    Initial check-in from Group 3

---
+++ HelloWorldFromGroup3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldFromGroup3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from Shyam Attiganal!");
+        }
+    }
+}
